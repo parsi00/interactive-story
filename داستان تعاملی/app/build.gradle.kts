17ac1fda6607a0d6f365b8169de06ad75d237dc6
parsi00 <parsabahrami644@gmail.com>
2026-07-22 21:29:15 +0330

    Update build.gradle.kts

--- داستان تعاملی/app/build.gradle.kts
+++ داستان تعاملی/app/build.gradle.kts
@@ -133,6 +133,4 @@ dependencies {
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  ksp(libs.androidx.room.compiler)
-  ksp(libs.moshi.kotlin.codegen)
 }
